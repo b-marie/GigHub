e9520744de581809f2383a14eb84690efbe5728d
Brittany Heller <brittany.heller@live.com>
2018-04-02 14:29:41 -0600

    Implement following, view gigs I'm attending

---
+++ GigHub/ViewModels/GigsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using GigHub.Models;
+using System.Linq;
+
+namespace GigHub.ViewModels
+{
+    public class GigsViewModel
+    {
+        public IQueryable<Gig> UpcomingGigs { get; set; }
+        public bool ShowActions { get; set; }
+        public string Heading { get; set; }
+    }
+}
